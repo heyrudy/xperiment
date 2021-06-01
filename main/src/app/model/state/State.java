30b0828049d9a76ad51124c38f3b8f55b7c6661a
Turing00 <kokou.koumagnagottoh@gmail.com>
2021-06-01 22:07:30 +0200

    refactor: update state field mutate function

--- main/src/app/model/state/State.java
+++ main/src/app/model/state/State.java
@@ -4,5 +4,13 @@ public interface State {
 
     default Object queryField(String name) throws NoSuchFieldException, IllegalAccessException {
         return name.equals(this.getClass().getField(name).getName()) ? this.getClass().getField(name).get(this) : null;
-    };
+    }
+
+    default void mutateField(String name, Object value) throws NoSuchFieldException, IllegalAccessException {
+        if (name.equals(this.getClass().getField(name).getName())) {
+            this.getClass().getField(name).set(this, value);
+        } else {
+            this.getClass().getField(name).set(this, this.getClass().getField(name).get(this));
+        }
+    }
 }
