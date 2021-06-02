4f69b43e75e3e142abb239bd96fadb2737d9d891
Turing00 <kokou.koumagnagottoh@gmail.com>
2021-06-02 11:21:37 +0200

    refactor: better support query and mutation on state

--- main/src/app/model/Todo.java
+++ main/src/app/model/Todo.java
@@ -7,7 +7,43 @@ import java.util.List;
 
 public final record Todo(Integer id, String text, Boolean completed) implements State {
 
-    public static List<Todo> init() {
+    public static List<Todo> initState() {
         return Collections.emptyList();
+    }
+
+    public Todo withId(Integer id) {
+        return new Todo(id, text(), completed());
+    }
+
+    public Todo withText(String text) {
+        return new Todo(id(), text, completed());
+    }
+
+    public Todo withCompleted(Boolean completed) {
+        return new Todo(id(), text(), completed);
+    }
+
+    public Object queryField(String name) {
+        if (name.equals("id")) {
+            return this.id();
+        } else if (name.equals("text")) {
+            return this.text();
+        } else if (name.equals("completed")) {
+            return this.completed();
+        } else {
+            return null;
+        }
+    }
+
+    public Object mutateField(String name, Object value) {
+        if (name.equals("id")) {
+            return this.withId((Integer) value);
+        } else if (name.equals("text")) {
+            return this.withText((String) value);
+        } else if (name.equals("completed")) {
+            return this.withCompleted((Boolean) value);
+        } else {
+            return this;
+        }
     }
 }
--- main/src/app/model/state/State.java
+++ main/src/app/model/state/State.java
@@ -2,15 +2,7 @@ package app.model.state;
 
 public interface State {
 
-    default Object queryField(String name) throws NoSuchFieldException, IllegalAccessException {
-        return name.equals(this.getClass().getField(name).getName()) ? this.getClass().getField(name).get(this) : null;
-    }
+    Object queryField(String name) ;
 
-    default void mutateField(String name, Object value) throws NoSuchFieldException, IllegalAccessException {
-        if (name.equals(this.getClass().getField(name).getName())) {
-            this.getClass().getField(name).set(this, value);
-        } else {
-            this.getClass().getField(name).set(this, this.getClass().getField(name).get(this));
-        }
-    }
+    Object mutateField(String name, Object value);
 }
